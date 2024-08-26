bb2d1a8a18e78fd651cda6e55025e7982ab3d796
Pankaj Patil <PPatil@stridely.local>
2024-08-26 16:45:10 +0530

    User Dashboard  Folders created

---
+++ Frontend/src/User-Dashboard/Table/TableContent/tablecontent.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tablecontent',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tablecontent.component.html',
+  styleUrls: ['./tablecontent.component.css']
+})
+export class TablecontentComponent {
+
+}
