ff3c06971483a5900645458608ffcec2b574e3a2
Pankaj Patil <PPatil@stridely.local>
2024-08-27 12:10:22 +0530

    AdminDashboard created

---
+++ Frontend/src/Admin-Dashboard/requests-table/requests-table.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-requests-table',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './requests-table.component.html',
+  styleUrls: ['./requests-table.component.css']
+})
+export class RequestsTableComponent {
+
+}
