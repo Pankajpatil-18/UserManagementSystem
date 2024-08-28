9051db1016c132dcfc0d66db71214f36ed09d73b
Aditiisb <bangaladiti61@gmail.com>
2024-08-28 15:03:12 +0530

    Aditi's commit 3

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b9eb3630a2d0f327ffbe96fcb4c0a8f580cb7b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d8bbf776e26ce3106bb08dc06f96f2fa4197003")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
