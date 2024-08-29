12a14e03806df4dcd2af8c574f8739dca1068dd1
Gayatri Gutte <GGutte@stridely.local>
2024-08-29 16:50:47 +0530

    tp

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eec8d809d3d4116a9ede0212a981c563f5e80d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9cbc756f96f431d3d4fd1bec42fb30e129528e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
