e5e5b468fb6615850fe9d500f9a5698f56726197
Pankaj Patil <PPatil@stridely.local>
2024-08-30 15:27:46 +0530

    Aditit

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2061dd777b002794d697dc28c649a2a18ecefe6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2bc93390084159193eca90934d9c00337b8bede")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
