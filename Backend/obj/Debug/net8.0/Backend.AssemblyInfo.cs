5e56d7053e03b86174d7d465efabf9ee79e97a9d
Pankaj Patil <PPatil@stridely.local>
2024-09-05 18:14:35 +0530

    ok:

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4776c72487f056aa7c1e32229036de01783b6aeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+962aafb5f65da3d15ac0610a9d089749dbda100b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
